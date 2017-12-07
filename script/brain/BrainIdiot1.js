a93896a82fe762f53ea23085608d50fe58666c41
Léon <leon.lenclos@gmail.com>
2017-12-07 09:00:24 +0100

    create a brain directory

--- script/brain/BrainIdiot1.js
+++ script/brain/BrainIdiot1.js
@@ -1,16 +1,12 @@
 function Brain () {
 
-
-	this.init = function() {
-		
+	this.init = function() {	
 	};
 
 	this.initFrom = function(parentBrain) {
-		
 	};
 	
 	this.choose = function(env) {
 		return floor(random(4));
 	};
 }
-
